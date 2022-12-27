bf103ba831c510022703c1bb9b949d0b861da14d
prko91 <prko911221@gmail.com>
2022-12-27 13:13:14 +0900

    commit

---
+++ javascript/kpr.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+$('.navi > li').mouseover(function () {
+$(this).find('.submenu').stop().slideDown(500);
+}).mouseout(function () {
+$(this).find('.submenu').stop().slideUp(500);
+});
+
+$(".notice li:first").click(function () {
+$("#modal").addClass("active");
+});
+
+var btn = document.querySelectorAll('.swapBtn')
+var slides = document.querySelectorAll('.slide').length - 1
+var slot = 0
+
+btn[0].addEventListener('click', function (e) {
+if (play) {
+  clearInterval(play)
+}
+var e = document.querySelector('.showing')
+if (slot == 0) {
+  slot = 3
+  document.querySelectorAll('.slide')[slides].classList.add('showing')
+  e.classList.remove('showing')
+} else {
+  slot--
+  e.previousElementSibling.classList.add('showing')
+  e.classList.remove('showing')
+}
+})
+btn[1].addEventListener('click', function (e) {
+if (play) {
+  clearInterval(play)
+}
+var e = document.querySelector('.showing')
+
+if (slot == slides) {
+  slot = 0
+  document.querySelectorAll('.slide')[0].classList.add('showing')
+  e.classList.remove('showing')
+} else {
+  slot++
+  e.nextElementSibling.classList.add('showing')
+  e.classList.remove('showing')
+}
+})
+function autoPlay() {
+e = document.querySelector('.showing')
+if (slot == slides) {
+  slot = 0
+  document.querySelectorAll('.slide')[0].classList.add('showing')
+  e.classList.remove('showing')
+} else {
+  slot++
+  e.nextElementSibling.classList.add('showing')
+  e.classList.remove('showing')
+}
+}
+var play = setInterval(autoPlay, 5000)
+});
